c64e54938247d9fae342dcc86d87e3565eb34641
selman <gunduzgece546@gmail.com>
2022-08-02 22:55:13 +0300

    blogcard

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,20 +1,24 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { blueGrey } from '@mui/material/colors';
-import Divider from '@mui/material/Divider';
-import { Box } from '@mui/system';
-const BlogCard = ({card}) => {
-  const dateLocal = new Date().toDateString().split(' ')
-const email ='walter@clarusway.com'
-const timeLocal = new Date().toLocaleTimeString();
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { Button, CardActionArea, CardActions } from '@mui/material'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import { blueGrey, grey, red } from '@mui/material/colors'
+
+import { Box } from '@mui/system'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
+
+const BlogCard = ({ card }) => {
+  const dateLocal = card.dateLocal.split(' ')
+
+  const email = 'walter@clarusway.com'
+  const timeLocal = new Date().toLocaleTimeString()
 
   return (
-    <Card sx={{ maxWidth: 345 }} >
+    <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
@@ -23,32 +27,66 @@ const timeLocal = new Date().toLocaleTimeString();
           alt="green iguana"
         />
         <CardContent>
-          <Typography sx={{color:'primary.light',fontFamily:'Girassol'}} gutterBottom variant="h5" component="h5">
+          <Typography
+            sx={{ color: 'primary.light', fontFamily: 'Girassol' }}
+            gutterBottom
+            variant="h5"
+            component="h5"
+          >
             {card.title}
           </Typography>
-          <Typography variant="" component="p" sx={{color:blueGrey[200],fontSize:'12px',marginBottom:'3px'}}>
-  {dateLocal[1]}, {dateLocal[0]}, {dateLocal[2]} {dateLocal[3]} {timeLocal[0]}{timeLocal[1]}{timeLocal[2]}{timeLocal[3]}{timeLocal[4]}{timeLocal[5]}{timeLocal[6]}{timeLocal[7]}{timeLocal[8]}{timeLocal[9]}{timeLocal[10]}
-</Typography>
-          
-          <Typography   sx={{
-      overflow: 'hidden',
-      textOverflow: 'ellipsis',
-      display: '-webkit-box',
-      WebkitLineClamp: '2',
-      WebkitBoxOrient: 'vertical',
-   }}  variant="body2" color="text.secondary">
-            {card.description}
+          <Typography
+            variant=""
+            component="p"
+            sx={{ color: blueGrey[200], fontSize: '12px', marginBottom: '3px' }}
+          >
+            {dateLocal[1]}, {dateLocal[0]}, {dateLocal[2]} {dateLocal[3]}{' '}
+            {timeLocal[0]}
+            {timeLocal[1]}
+            {timeLocal[2]}
+            {timeLocal[3]}
+            {timeLocal[4]}
+            {timeLocal[5]}
+            {timeLocal[6]}
+            {timeLocal[7]}
+            {timeLocal[8]}
+            {timeLocal[9]}
+            {timeLocal[10]}
           </Typography>
-         
-          <Box sx={{display:'flex'}}>
-
-          <Typography >
-            <AccountCircleIcon/>
-          </Typography>  <Typography>{email} </Typography> 
-          </Box>
-     
+          <Typography
+            sx={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              display: '-webkit-box',
+              WebkitLineClamp: '2',
+              WebkitBoxOrient: 'vertical',
+            }}
+            component={'p'}
+            variant="body2"
+            color="text.secondary"
+          >
+            {card.description}
+          </Typography>{' '}
         </CardContent>
       </CardActionArea>
+      <CardActions>
+        <Box sx={{ display: 'flex', cursor: 'default' }}>
+          <Typography>
+            <AccountCircleIcon />
+          </Typography>{' '}
+          <Typography>{email} </Typography>
+        </Box>
+      </CardActions>
+      <CardActions sx={{ color: grey[400], fontSize: '12px', gap: 1 }}>
+        {card.like ? (
+          <FavoriteIcon sx={{ cursor: 'pointer', color: red[500] }} />
+        ) : (
+          <FavoriteIcon sx={{ cursor: 'pointer' }} />
+        )}
+        <Typography>{card.like}</Typography>{' '}
+        <ChatBubbleOutlineIcon sx={{ cursor: 'pointer' }} />
+        <Typography>{card.comments.count}</Typography>
+      </CardActions>
     </Card>
   )
 }
--- src/helper/data.js
+++ src/helper/data.js
@@ -1,104 +1,122 @@
 export const data = [
   {
-    id:1,
-    title:'Lorem İpsum',
-    imgUrl:'https://learnenglishteens.britishcouncil.org/sites/teens/files/styles/article/public/istock_000020473404small.jpg?itok=t-nIMAiJ',
-    description:'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
-    like:2,
-    comments:{
-      count:4,
-      comment:[
+    id: 1,
+    title: 'Lorem İpsum',
+    imgUrl:
+      'https://learnenglishteens.britishcouncil.org/sites/teens/files/styles/article/public/istock_000020473404small.jpg?itok=t-nIMAiJ',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
+    like: 6,
+    timeLocal: '17:23:29',
+    dateLocal: 'Tue Jul 02 2022',
+    comments: {
+      count: 4,
+      comment: [
         {
           commentDesc: 'Lorem ipsum dolor sit amet, consect',
-          commenter:'ali'
-
-        }
-      ]
-    }
+          commenter: 'ali',
+        },
+      ],
+    },
   },
   {
-    id:2,
-    title:'Lorem İpsum',
-    imgUrl:'https://www.timeshighereducation.com/sites/default/files/styles/the_breaking_news_image_style/public/languages-signpost.jpg?itok=VSbR4uZ9',
-    description:'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
-    like:2,
-    comments:{
-      count:4,
-      comment:[
+    id: 2,
+    title: 'Lorem İpsum',
+    imgUrl:
+      'https://www.timeshighereducation.com/sites/default/files/styles/the_breaking_news_image_style/public/languages-signpost.jpg?itok=VSbR4uZ9',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
+    like: 6,
+    timeLocal: '00:23:29',
+    dateLocal: 'Sun Aug 02 2022',
+    comments: {
+      count: 2,
+      comment: [
         {
           commentDesc: 'Lorem ipsum dolor sit amet, consect',
-          commenter:'ali'
-
-        }
-      ]
-    }
+          commenter: 'ali',
+        },
+      ],
+    },
   },
   {
-    id:3,
-    title:'Lorem İpsum',
-    imgUrl:'https://learnenglishteens.britishcouncil.org/sites/teens/files/styles/article/public/istock_000020473404small.jpg?itok=t-nIMAiJ',
-    description:'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
-    like:2,
-    comments:{
-      count:4,
-      comment:[
+    id: 3,
+    title: 'Lorem İpsum',
+    imgUrl:
+      'https://learnenglishteens.britishcouncil.org/sites/teens/files/styles/article/public/istock_000020473404small.jpg?itok=t-nIMAiJ',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
+    like: 4,
+    timeLocal: '17:30:20',
+    dateLocal: 'Tue Aug 02 2022',
+    comments: {
+      count: 0,
+      comment: [
         {
           commentDesc: 'Lorem ipsum dolor sit amet, consect',
-          commenter:'ali'
-
-        }
-      ]
-    }
+          commenter: 'ali',
+        },
+      ],
+    },
   },
   {
-    id:4,
-    title:'Lorem İpsum',
-    imgUrl:'https://sp-ao.shortpixel.ai/client/to_auto,q_lossy,ret_img,w_1024,h_724/https://localizationlab.com/wp-content/uploads/2021/05/IMAGE_TRANSLATION-8-1024x724.png',
-    description:'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
-    like:2,
-    comments:{
-      count:4,
-      comment:[
+    id: 4,
+    title: 'Lorem İpsum',
+    imgUrl:
+      'https://sp-ao.shortpixel.ai/client/to_auto,q_lossy,ret_img,w_1024,h_724/https://localizationlab.com/wp-content/uploads/2021/05/IMAGE_TRANSLATION-8-1024x724.png',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
+    like: 0,
+    timeLocal: '17:23:29',
+    dateLocal: 'Tue Aug 02 2022',
+    comments: {
+      count: 0,
+      comment: [
         {
           commentDesc: 'Lorem ipsum dolor sit amet, consect',
-          commenter:'ali'
-
-        }
-      ]
-    }
+          commenter: 'ali',
+        },
+      ],
+    },
   },
   {
-    id:5,
-    title:'Lorem İpsum',
-    imgUrl:'https://upload.wikimedia.org/wikipedia/commons/c/c8/Human_Language_Families.png',
-    description:'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
-    like:2,
-    comments:{
-      count:4,
-      comment:[
+    id: 5,
+    title: 'Lorem İpsum',
+    imgUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/c/c8/Human_Language_Families.png',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
+    like: 3,
+    timeLocal: '17:23:29',
+    dateLocal: 'Tue Aug 02 2022',
+    comments: {
+      count: 1,
+      comment: [
         {
           commentDesc: 'Lorem ipsum dolor sit amet, consect',
-          commenter:'ali'
-
-        }
-      ]
-    }
+          commenter: 'ali',
+        },
+      ],
+    },
   },
   {
-    id:6,
-    title:'Lorem İpsum',
-    imgUrl:'https://media.baamboozle.com/uploads/images/113260/1605442630_235123',
-    description:'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
-    like:2,
-    comments:{
-      count:4,
-      comment:[
+    id: 6,
+    title: 'Lorem İpsum',
+    imgUrl:
+      'https://media.baamboozle.com/uploads/images/113260/1605442630_235123',
+    description:
+      'lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip ',
+    like: 12,
+    timeLocal: '17:23:29',
+    dateLocal: 'Tue Aug 02 2022',
+    comments: {
+      count: 2,
+      comment: [
         {
           commentDesc: 'Lorem ipsum dolor sit amet, consect',
-          commenter:'ali'
-
-        }
-      ]
-    }
-  }
+          commenter: 'ali',
+        },
+      ],
+    },
+  },
 ]
